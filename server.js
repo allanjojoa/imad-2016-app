a0f96cc83acb44b63a72b5ddc3303ec9660153ab
allanjojoa <allanjojoa@gmail.com>
2016-09-22 22:46:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req,res){
     //articleName=Article-one or artcile-two
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
