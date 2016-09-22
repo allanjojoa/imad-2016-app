a80a496ef0327b961e6ac39c99fdac27250a6ca1
allanjojoa <allanjojoa@gmail.com>
2016-09-22 22:48:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,15 +91,6 @@ app.get('/:articleName', function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function(req,res){
-    res.send('Article Two is requested and will be server.');
-});
-
-app.get('/article-three', function(req,res){
-    res.send('Article Three is requested and will be server.');
-});
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
