810a1a64745616af4967f6d9afd5f468bac0f71f
allanjojoa <allanjojoa@gmail.com>
2016-09-22 20:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function(req,res){
+app.get('/article-one', function(req,res){
     res.send('Article One is requested and will be server.');
 });
 
-app.get('article-two', function(req,res){
+app.get('/article-two', function(req,res){
     res.send('Article Two is requested and will be server.');
 });
 
-app.get('article-three', function(req,res){
+app.get('/article-three', function(req,res){
     res.send('Article Three is requested and will be server.');
 });
 
