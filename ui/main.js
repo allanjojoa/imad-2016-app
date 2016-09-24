52c41f5b1d3157cb6a799201535525e75130f2c4
allanjojoa@gmail.com <allanjojoa@gmail.com>
2016-09-24 23:40:34 +0530

    Modue_p5

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+//chnage HTML
+var element=document.getElementById('main-text');
+element.innerHTML='New Value';
+
+//move image
+var img=document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft=marginLeft+'px';
+};
+img.onclick=function(){
+    var interval =  setInterval(moveRight,50);
+};
