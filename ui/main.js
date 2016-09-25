8745c75adc3babae8b78e0f042e7dc03addcce83
allanjojoa <allanjojoa@gmail.com>
2016-09-25 15:19:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,6 +45,6 @@ submit.onclick=function(){
     
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
-    request.open('GET','http://localhost:8080/submit-name?name='+name,true);
+    request.open('GET','http://allanjojoa.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
