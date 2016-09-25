cf5195230c9ef4e5b79bc0f0cc2fac96fc72de66
allanjojoa <allanjojoa@gmail.com>
2016-09-25 15:08:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,14 @@ app.get('/counter', function (req, res){
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name=req.query.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function(req,res){
     //articleName=Article-one or artcile-two
     var articleName = req.params.articleName;
