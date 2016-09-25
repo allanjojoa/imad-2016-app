2ba489da4633e4d1c3ae9836c595ff39b8044515
allanjojoa <allanjojoa@gmail.com>
2016-09-25 12:45:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,5 @@ button.onclick = function(){
 
 
     request.open('GET','http://allanjojoa.imad.hasura-app.io/counter',true);
-    request.open('GET','http://localhost:8080/counter',true);
     request.send(null);
 };
