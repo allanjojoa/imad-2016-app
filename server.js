0affe31266fd512405dcd57b3dbf6a1f19cb745b
allanjojoa <allanjojoa@gmail.com>
2016-09-25 11:51:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/counter', function (req,res){
+app.get('/counter', function (req, res){
     counter=counter+1;
     res.send(counter.toString());
 });
